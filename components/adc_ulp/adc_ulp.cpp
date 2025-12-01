ab53815a058eebd61a0b22edfbe8931274687379
Jonas Kello <jonas.kello@gmail.com>
2025-12-01 12:35:45 +0100

    cold boot

--- components/adc_ulp/adc_ulp.cpp
+++ components/adc_ulp/adc_ulp.cpp
@@ -43,76 +43,72 @@ void ADCULPSensor::setup() {
     // Stop any previously running ULP program
     ulp_timer_stop();
 
-    // Init ADC unit
-    adc_oneshot_unit_handle_t adc1_handle;
-    adc_oneshot_unit_init_cfg_t init_cfg = { .unit_id = ADC_UNIT_1, .ulp_mode = ADC_ULP_MODE_FSM };
-    esp_err_t err_unit = adc_oneshot_new_unit(&init_cfg, &adc1_handle);
-    if (err_unit != ESP_OK) {
-        ESP_LOGE(TAG, "Error initializing ADC1: %d", err_unit);
-        this->mark_failed();
-        return;
-    }
-    this->setup_flags_.handle_init_complete = true;
-
-    // Init ADC channel 
-    adc_oneshot_chan_cfg_t chan_cfg = { .atten = ADC_ATTEN_DB_12, .bitwidth = ADC_BITWIDTH_12 };
-    esp_err_t err_channel = adc_oneshot_config_channel(adc1_handle, channel_, &chan_cfg);
-    if (err_channel != ESP_OK) {
-        ESP_LOGE(TAG, "Error configuring channel: %d", err_channel);
-        this->mark_failed();
-        return;
-    }
-    this->setup_flags_.config_complete = true;
-
-    // Prime baseline with one CPU-side read
-    int baseline = 0;
-    // esp_err_t err_read = adc_oneshot_read(adc1_handle, channel_, &baseline);
-    // if (err_read != ESP_OK) {
-    //     ESP_LOGE(TAG, "Error reading channel: %d", err_read);
-    //     this->mark_failed();
-    //     return;
-    // }
-    // this->setup_flags_.calibration_complete = true;
-    // ESP_LOGI(TAG, "Primed baseline with initial ADC value: %d", baseline);
-
-    // If this is the first power on (not wakeup), load the RTC with inital values 
+    // If this is the first power on (not wakeup), then initialize the ULP 
     if(esp_sleep_get_wakeup_cause() != ESP_SLEEP_WAKEUP_ULP) {
+
+        ESP_LOGI(TAG, "First power on, init ULP...");
+
+        // Init ADC unit for use with ULP
+        adc_oneshot_unit_handle_t adc1_handle;
+        adc_oneshot_unit_init_cfg_t init_cfg = { .unit_id = ADC_UNIT_1, .ulp_mode = ADC_ULP_MODE_FSM };
+        esp_err_t err_unit = adc_oneshot_new_unit(&init_cfg, &adc1_handle);
+        if (err_unit != ESP_OK) {
+            ESP_LOGE(TAG, "Error initializing ADC1: %d", err_unit);
+            this->mark_failed();
+            return;
+        }
+        this->setup_flags_.handle_init_complete = true;
+
+        // Init ADC channel for use with ULP
+        adc_oneshot_chan_cfg_t chan_cfg = { .atten = ADC_ATTEN_DB_12, .bitwidth = ADC_BITWIDTH_12 };
+        esp_err_t err_channel = adc_oneshot_config_channel(adc1_handle, channel_, &chan_cfg);
+        if (err_channel != ESP_OK) {
+            ESP_LOGE(TAG, "Error configuring channel: %d", err_channel);
+            this->mark_failed();
+            return;
+        }
+        this->setup_flags_.config_complete = true;
+
         // Set baseline out of range baseline to trigger first value publish
         RTC_SLOW_MEM[DATA_BASE_SLOT + BASELINE_OFFSET] = 0x7FFF;
+        // Prevent measurements when CPU is awake by setting ARM = 0
+        RTC_SLOW_MEM[DATA_BASE_SLOT + ARM_OFFSET] = 0;
         RTC_SLOW_MEM[DATA_BASE_SLOT + DEBUG1_OFFSET] = 1;
         RTC_SLOW_MEM[DATA_BASE_SLOT + DEBUG2_OFFSET] = 2;
-        ESP_LOGI(TAG, "First power on, loaded intial values into RTC");
+
     }
 
-    // Run ULP
+    // Define ULP program
     const ulp_insn_t ulp_prog[] = {
 
-        I_MOVI(R3, DATA_BASE_SLOT),        // R3 = base pointer
+        // Use R3 as data pointer in the whole program
+        I_MOVI(R3, DATA_BASE_SLOT), // R3 = base data pointer
 
         // Check ARM flag so we don't measure values when the CPU is awake
-        I_LD(R0, R3, ARM_OFFSET),   // R0 = ARM
-        M_BL(2, 1),             // if ARM<1 it is 0 (it can be 1 or 0) → branch to label 2 (skip)
+        I_LD(R0, R3, ARM_OFFSET),  // R0 = ARM
+        M_BL(2, 1),                // if ARM < 1 it is 0 (it can be 1 or 0) → branch to label 2 (skip)
 
+        // Run real program when armed
         I_ADC(R2, 0, (uint32_t)channel_),  // R2 = raw ADC
-        I_LD(R1, R3, BASELINE_OFFSET), // R1 = baseline
+        I_LD(R1, R3, BASELINE_OFFSET),     // R1 = baseline
         I_SUBR(R0, R2, R1),                // R0 = raw - baseline
-    I_ST(R1, R3, DEBUG1_OFFSET),  // DEBUG
-    I_ST(R2, R3, DEBUG2_OFFSET),  // DEBUG
+        // I_ST(R1, R3, DEBUG1_OFFSET),  // DEBUG
+        // I_ST(R2, R3, DEBUG2_OFFSET),  // DEBUG
         M_BGE(1, threshold_),              // if diff >= threshold goto wake
         I_SUBR(R0, R1, R2),                // R0 = baseline - raw
         M_BGE(1, threshold_),              // if diff >= threshold goto wake
         M_BX(2),                           // else skip wake
         M_LABEL(1),
-            I_WAKE(),                       // wake CPU
             I_ST(R2, R3, BASELINE_OFFSET),  // update baseline with raw ADC
-            I_MOVI(R0, 0),                  // clear ARM
-            I_ST(R0, R3, ARM_OFFSET),
+            I_MOVI(R0, 0),                  // R0 = 0 to clear ARM
+            I_ST(R0, R3, ARM_OFFSET),       // clear ARM before we wake the cpu so we don't run while it is awake
+            I_WAKE(),                       // wake CPU
         M_LABEL(2),
-            I_HALT()                           // halt
+            I_HALT()                        // halt
     };
 
-    // Microseconds to delay between halt and wake states
-    esp_err_t r = ulp_set_wakeup_period(0, 1000 * 1000);
+    // Microseconds to delay between ULP halt and wake states
+    esp_err_t r = ulp_set_wakeup_period(0, 100 * 1000);
     if (r != ESP_OK) {
         ESP_LOGE(TAG, "ulp_set_wakeup_period failed: %d", r);
         this->mark_failed();
@@ -137,6 +133,9 @@ void ADCULPSensor::setup() {
     esp_sleep_enable_ulp_wakeup();
     esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON);
 
+    ESP_LOGI(TAG, "First power on, init ULP completed...");
+
+
     this->setup_flags_.init_complete = true;
 }
 
@@ -158,7 +157,7 @@ void ADCULPSensor::loop() {
     // Immediately go back to deep sleep
     ESP_LOGI(TAG, "Entering deep sleep until next ULP wake...");
     delay(100);  // give logger time to flush
-    RTC_SLOW_MEM[DATA_BASE_SLOT + ARM_OFFSET] = 1;
+    RTC_SLOW_MEM[DATA_BASE_SLOT + ARM_OFFSET] = 1; // Tell ULP to start making measurements
     esp_deep_sleep_start();
 }
 
